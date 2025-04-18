35eb787dc3cf940dedfb875d8f23b3427cd7016d
Sarvani Balivada <balivadaaruna2@gmail.com>
2025-04-18 20:03:25 +0530

    Set Login as default page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome to WasteLoop</h1>
-        <p>Your platform for sustainable waste management and upcycling ideas.</p>
-        <a
-          className="App-link"
-          href="https://renewwaste.netlify.app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Explore More
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          {/* Add other routes here if needed */}
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
