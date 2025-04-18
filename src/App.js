15b8c605b2f743e8e3252307134fa59d3333ade3
Sarvani Balivada <balivadaaruna2@gmail.com>
2025-04-18 20:36:18 +0530

    Add Login route to App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Login from './pages/Login';
+import Navbar from './components/Navbar'; // Import Navbar component
+import Home from './pages/Home'; // Import Home component
+import SellItems from './pages/SellItems'; // Import SellItems component
+import UpcycleIdeas from './pages/UpcycleIdeas'; // Import UpcycleIdeas component
+import Login from './pages/Login'; // Import Login component
+import Marketplace from './pages/Marketplace'; // Import Marketplace component
 
 function App() {
   return (
     <Router>
       <div className="App">
+        <Navbar /> {/* Include Navbar here */}
         <Routes>
-          <Route path="/" element={<Login />} />
+          <Route path="/" element={<Home />} /> {/* Define Home route */}
+          <Route path="/login" element={<Login />} /> {/* Define Login route */}
+          <Route path="/marketplace" element={<Marketplace />} /> {/* Define Marketplace route */}
+          <Route path="/sell" element={<SellItems />} /> {/* Define SellItems route */}
+          <Route path="/upcycle-ideas" element={<UpcycleIdeas />} /> {/* Define UpcycleIdeas route */}
           {/* Add other routes here if needed */}
         </Routes>
       </div>
